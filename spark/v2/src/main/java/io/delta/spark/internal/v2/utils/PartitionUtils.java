086cdef7d85ca805d1fca0dfcd131f8144080e9c
Sandro Sp <sandrospeh@gmail.com>
2026-05-13 15:40:58 +0000

    Mostly linting/formatting

--- spark/v2/src/main/java/io/delta/spark/internal/v2/utils/PartitionUtils.java
+++ spark/v2/src/main/java/io/delta/spark/internal/v2/utils/PartitionUtils.java
@@ -23,11 +23,6 @@ import io.delta.kernel.internal.actions.DeletionVectorDescriptor;
 import io.delta.kernel.internal.actions.Metadata;
 import io.delta.kernel.internal.actions.Protocol;
 import io.delta.kernel.internal.tablefeatures.TableFeatures;
-import org.apache.spark.sql.delta.RowIndexFilterType;
-import org.apache.spark.sql.delta.DefaultRowCommitVersion$;
-import org.apache.spark.sql.delta.DeltaColumnMapping;
-import org.apache.spark.sql.delta.DeltaParquetFileFormat;
-import org.apache.spark.sql.delta.RowId$;
 import io.delta.spark.internal.v2.read.DeltaParquetFileFormatV2;
 import io.delta.spark.internal.v2.read.SparkReaderFactory;
 import io.delta.spark.internal.v2.read.cdc.CDCReadFunction;
@@ -49,6 +44,11 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.connector.read.InputPartition;
 import org.apache.spark.sql.connector.read.PartitionReaderFactory;
+import org.apache.spark.sql.delta.DefaultRowCommitVersion$;
+import org.apache.spark.sql.delta.DeltaColumnMapping;
+import org.apache.spark.sql.delta.DeltaParquetFileFormat;
+import org.apache.spark.sql.delta.RowId$;
+import org.apache.spark.sql.delta.RowIndexFilterType;
 import org.apache.spark.sql.execution.datasources.FileFormat$;
 import org.apache.spark.sql.execution.datasources.FilePartition;
 import org.apache.spark.sql.execution.datasources.FilePartition$;
@@ -245,8 +245,15 @@ public class PartitionUtils {
       Configuration hadoopConf,
       SQLConf sqlConf) {
     return createDeltaParquetReaderFactory(
-        snapshot, dataSchema, partitionSchema, readDataSchema,
-        dataFilters, scalaOptions, hadoopConf, sqlConf, /* isCDCRead */ false);
+        snapshot,
+        dataSchema,
+        partitionSchema,
+        readDataSchema,
+        dataFilters,
+        scalaOptions,
+        hadoopConf,
+        sqlConf, /* isCDCRead */
+        false);
   }
 
   public static PartitionReaderFactory createDeltaParquetReaderFactory(
